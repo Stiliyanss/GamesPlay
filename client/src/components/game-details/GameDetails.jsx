140a16831834967644560cc10b4e9f91cd55897d
Stiliyanss <stilianss03@gmail.com>
2025-03-25 01:16:22 +0200

    extract comments jsx in separete components

--- client/src/components/game-details/GameDetails.jsx
+++ client/src/components/game-details/GameDetails.jsx
@@ -2,6 +2,8 @@ import { useEffect } from "react";
 import { useState } from "react";
 import { Link, useNavigate, useParams } from "react-router";
 import gameService from "../../services/gameService";
+import CommentsShow from "../comments-show/CommentsShow";
+import CommentsCreate from "../comments-create/CommentsCreate";
 
 export default function GameDetails() {
     const [game,setGame] = useState({});
@@ -39,20 +41,7 @@ export default function GameDetails() {
               {game.summary}
           </p>
               {/* <!-- Bonus ( for Guests and Users ) --> */}
-        <div className="details-comments">
-            <h2>Comments:</h2>
-            <ul>
-                {/* <!-- list all comments for current game (If any) --> */}
-                <li className="comment">
-                    <p>Content: I rate this one quite highly.</p>
-                </li>
-                <li className="comment">
-                    <p>Content: The best game.</p>
-                </li>
-            </ul>
-            {/* <!-- Display paragraph: If there are no games in the database --> */}
-            <p className="no-comment">No comments.</p>
-        </div>
+                <CommentsShow/>
 
         {/* <!-- Edit/Delete buttons ( Only for creator of this game )  --> */}
         <div className="buttons">
@@ -63,13 +52,7 @@ export default function GameDetails() {
 
     {/* <!-- Bonus --> */}
     {/* <!-- Add Comment ( Only for logged-in users, which is not creators of the current game ) --> */}
-    <article className="create-comment">
-        <label>Add new comment:</label>
-        <form className="form">
-            <textarea name="comment" placeholder="Comment......"></textarea>
-            <input className="btn submit" type="submit" defaultValue="Add Comment" />
-        </form>
-    </article>
+    <CommentsCreate/>
 
 </section>
     );
