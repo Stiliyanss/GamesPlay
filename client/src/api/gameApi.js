345bdde2f873e768746148ac8c71f94789abce9f
Stiliyanss <stilianss03@gmail.com>
2025-03-28 18:00:39 +0200

    Create game with collections service

---
+++ client/src/api/gameApi.js
@@ -0,0 +1,42 @@
+import { useContext } from "react";
+import request from "../utils/request";
+import { UserContext } from "../contexts/userContext";
+
+const baseUrl = 'http://localhost:3030/data/games';
+
+export default {
+   
+  async getAll(){
+    const result = await request.get(baseUrl);
+
+    const games = Object.values(result);
+
+    return games;
+  },
+  async getOne(gameId){
+    return request.get(`${baseUrl}/${gameId}`)
+  },
+  delete(gameId){
+    return request.delete(`${baseUrl}/${gameId}`)
+  },
+  edit(gameId, gameData){
+    return request.put(`${baseUrl}/${gameId}`, {...gameData, _id:gameId})
+  }
+};
+
+export const useCreateGame = () =>{
+  const {accessToken} = useContext(UserContext);
+
+  const options = {
+    headers:{
+      'X-Authorization':accessToken
+    }
+  }
+
+  const create = (gameData)=> request.post(baseUrl,gameData);
+
+
+  return{
+    create,
+  }
+}
--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -1,22 +1,28 @@
+import { useContext } from "react";
 import { Link } from "react-router";
+import { UserContext } from "../../contexts/userContext";
 
 export default function Header() {
+const {email} = useContext(UserContext);
+
     return(
 <header>
     {/* <!-- Navigation --> */}
     <h1><Link className="home" to="/">GamesPlay</Link></h1>
     <nav>
         <Link to="/games">All games</Link>
-        {/* <!-- Logged-in users --> */}
-        <div id="user">
+        {email ?
+        ( <div id="user">
             <Link to="/games/create">Create Game</Link>
             <Link to="/logout">Logout</Link>
-        </div>
-        {/* <!-- Guest users --> */}
-        <div id="guest">
+            {email}
+        </div>) :
+        (<div id="guest">
             <Link to="/login">Login</Link>
             <Link to="/register">Register</Link>
-        </div>
+        </div>)}
+       
+        
     </nav>
 </header>
   );
